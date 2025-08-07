e13c5d49aa78b40773c54ce5c14406c98f035e98
Sawera <saweramukhtar221@gmail.com>
2025-08-07 12:18:21 +0500

    setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+def get_requirements(file_path: str) -> List[str]:
+    '''This function will return the list of requirements from the given file path'''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n", "") for req in requirements]
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+
+setup(
+    name='ML_Project',
+    version='0.0.1',
+    author='Sawera Mukhtar',
+    author_email="saweramukhtar221@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+)
